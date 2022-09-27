702be21a1f28f1c17db61774b1764e3b6f8f769d
Germain Mathis <mgermain@ca-cf.fr>
2022-09-28 01:33:13 +0200

    replace json character file by indivudal member file , map json du character , fonctionnalite statistiques

--- commands/define-stats.js
+++ commands/define-stats.js
@@ -12,56 +12,53 @@ module.exports = {
      * @param {CommandInteraction} interaction 
      */    
     async execute(interaction){
-        const modal = new ModalBuilder()
-			.setCustomId('defineStats')
-			.setTitle('Vos statistiques');
 
-		// Add components to modal
-		// Create the text input components
+		const fs = require("fs");
+		fs.readFile("./databases/Personnages/"+interaction.user.tag+".json", "utf8", (err, jsonString) => {
+		if (err) {
+			console.log("File read failed:", err);
+			return;
+		}else{
+			console.log("File data:", jsonString);
+			const modal = new ModalBuilder()
+				.setCustomId('defineStats')
+				.setTitle('Vos statistiques');
+				
 
-        const forceInput = new TextInputBuilder()
-		    .setCustomId('forceInput')
-			.setLabel("Force : ")
-		    // Paragraph means multiple lines of text.
-			.setStyle(TextInputStyle.Short); 
-            
-        const courageInput = new TextInputBuilder()
-		    .setCustomId('courageInput')
-			.setLabel("Courage : ")
-		    // Paragraph means multiple lines of text.
-			.setStyle(TextInputStyle.Short);
+			const forceInput = new TextInputBuilder()
+				.setCustomId('forceInput')
+				.setLabel("Force : ")
+				.setStyle(TextInputStyle.Short); 
+				
+			const courageInput = new TextInputBuilder()
+				.setCustomId('courageInput')
+				.setLabel("Courage : ")
+				.setStyle(TextInputStyle.Short);
 
-        const agilInput = new TextInputBuilder()
-		    .setCustomId('agilInput')
-			.setLabel("Agilité : ")
-		    // Paragraph means multiple lines of text.
-			.setStyle(TextInputStyle.Short);       
-            
-        const intelInput = new TextInputBuilder()
-		    .setCustomId('intelInput')
-			.setLabel("Intelligence : ")
-		    // Paragraph means multiple lines of text.
-			.setStyle(TextInputStyle.Short);     
+			const agilInput = new TextInputBuilder()
+				.setCustomId('agilInput')
+				.setLabel("Agilité : ")
+				.setStyle(TextInputStyle.Short);       
+				
+			const intelInput = new TextInputBuilder()
+				.setCustomId('intelInput')
+				.setLabel("Intelligence : ")
+				.setStyle(TextInputStyle.Short);     
 
-        const charismeInput = new TextInputBuilder()
-		    .setCustomId('charismeInput')
-			.setLabel("Charisme : ")
-		    // Paragraph means multiple lines of text.
-			.setStyle(TextInputStyle.Short);       
+			const charismeInput = new TextInputBuilder()
+				.setCustomId('charismeInput')
+				.setLabel("Charisme : ")
+				.setStyle(TextInputStyle.Short);       
 
-		// An action row only holds one text input,
-		// so you need one action row per text input.
-        const actionRow1 = new ActionRowBuilder().addComponents(courageInput);
-        const actionRow2 = new ActionRowBuilder().addComponents(forceInput);
-        const actionRow3 = new ActionRowBuilder().addComponents(agilInput);
-        const actionRow4 = new ActionRowBuilder().addComponents(intelInput);
-        const actionRow5 = new ActionRowBuilder().addComponents(charismeInput);
-        
-		// Add inputs to the modal
-		modal.addComponents(actionRow1, actionRow2,actionRow3,actionRow4,actionRow5);
-
-		// Show the modal to the user
-		await interaction.showModal(modal);
+			const actionRow1 = new ActionRowBuilder().addComponents(courageInput);
+			const actionRow2 = new ActionRowBuilder().addComponents(forceInput);
+			const actionRow3 = new ActionRowBuilder().addComponents(agilInput);
+			const actionRow4 = new ActionRowBuilder().addComponents(intelInput);
+			const actionRow5 = new ActionRowBuilder().addComponents(charismeInput);
+			modal.addComponents(actionRow1, actionRow2,actionRow3,actionRow4,actionRow5);
+			interaction.showModal(modal);
+		}
+		});
 
     }
 
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const { Client , Collection , GatewayIntentBits } = require('discord.js');
 const {token} = require('./config.json');
-const databases = {
-    characters: require('./databases/characters.json')
-};
 
 const fs = require('fs');
 
@@ -26,32 +23,80 @@ client.on('interactionCreate', async interaction => {
     if (interaction.isCommand()) handleCommand(client, interaction);
 
     if (interaction.customId === 'CreateCharacter') {
+        console.log(interaction.user.tag);
 		await interaction.reply({ content: 'Votre personnage à été créer' });
         const characterName = interaction.fields.getTextInputValue('nameInput');
 	    const characterRace = interaction.fields.getTextInputValue('raceInput');
         const characterClasse = interaction.fields.getTextInputValue('classeInput');
         const characterLevel = interaction.fields.getTextInputValue('levelInput');
 	    console.log({ characterName, characterRace, characterClasse, characterLevel });
-        saveCharacter(characterName,characterRace,characterClasse,characterLevel);
+        saveCharacter(characterName,characterRace,characterClasse,characterLevel,interaction.user.tag);
+	}
+
+    if (interaction.customId === 'defineStats') {
+        console.log(interaction.user.tag);
+		await interaction.reply({ content: 'Vos statistiques ont été définis' });
+        const characterForce = interaction.fields.getTextInputValue('forceInput');
+	    const characterCourage = interaction.fields.getTextInputValue('courageInput');
+        const characterAgil = interaction.fields.getTextInputValue('agilInput');
+        const characterIntel = interaction.fields.getTextInputValue('intelInput');
+        const characterCharisme = interaction.fields.getTextInputValue('charismeInput');
+        saveStats(characterForce,characterCourage,characterAgil,characterIntel,characterCharisme,interaction.user.tag);
 	}
 });
 
-function saveCharacter(name,race,classe,level){
-    databases.characters[name] = {
+function saveCharacter(name,race,classe,level,userId){
+    data = {
+        nom:name,
         race:race,
         classe:classe,
         niveau:level
     }
-    const data = JSON.stringify(databases.characters);
-    console.log("data")
-    fs.writeFile("./databases/characters.json", JSON.stringify(databases.characters),(err) => {
+    
+    console.log(data);
+
+    fs.writeFile("./databases/Personnages/"+userId+".json", JSON.stringify(data),(err) => {
         if (err)
           console.log(err);
         else {
           console.log("File written successfully\n");
+
         }
       });
     console.log("Personnage enregistré");
+}
+
+function saveStats(characterForce,characterCourage,characterAgil,characterIntel,characterCharisme,userId){
+    data = {
+        Force:characterForce,
+        Courage:characterCourage,
+        Adresse:characterAgil,
+        Intelligence:characterIntel,
+        Charisme:characterCharisme
+    }
+    
+    console.log(data);
+    fs.readFile("./databases/Personnages/"+userId+".json", "utf8", (err, jsonString) => {
+		if (err) {
+			console.log("File read failed:", err);
+			return;
+		}
+        console.log("File data:", jsonString);
+        const map = new Map(Object.entries(JSON.parse(jsonString)));
+        console.log("map : ", map);
+    });
+    /*
+    fs.writeFile("./databases/Personnages/"+userId+".json", JSON.stringify(data),(err) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("File written successfully\n");
+
+        }
+      });
+      */
+    console.log("Personnage enregistré");
+    
 }
 
 client.login(token);
